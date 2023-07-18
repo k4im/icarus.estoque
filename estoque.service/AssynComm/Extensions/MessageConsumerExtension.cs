16b4698fff4949f79954a690ba670c5420a0aff2
k4im <contato.vitorsantos@hotmail.com>
2023-07-18 10:43:36 -0300

    Adicionado exchange

--- estoque.service/AssynComm/Extensions/MessageConsumerExtension.cs
+++ estoque.service/AssynComm/Extensions/MessageConsumerExtension.cs
@@ -16,6 +16,12 @@ namespace estoque.service.AssynComm.Extensions
             // Definindo o balanceamento da fila para uma mensagem por vez.
             channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
+            // Definindo o Exchange no RabbitMQ
+            channel.ExchangeDeclare(exchange: exchange,
+            type: ExchangeType.Topic,
+            durable: true,
+            autoDelete: false);
+
             // Linkando a fila de eventos atualizados ao exchange
             channel.QueueBind(queue: filaAtualizarEstoque,
                 exchange: exchange,
